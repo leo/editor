824ee914385b4b4af7a902ce4ae994574c5b6d81
leo <leo@users.noreply.github.com>
2015-12-18 21:04:15 +0100

    Then let's start over!

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
 (function(){
 
+  window.addEventListener( 'load', () => {
+    if( document.contentEditable != undefined && document.execCommand != undefined ) {
+      alert( 'Get me HTML5!' );
+    } else {
+      document.execCommand( 'styleWithCSS', false, true );
+    }
+  }, false);
+
+  var underlineButton = document.querySelector( 'a[href="#underline"]' );
+
+  underlineButton.addEventListener( 'click', (event) => {
+    document.execCommand( 'underline', false, null );
+    event.preventDefault();
+  });
+
 })();
