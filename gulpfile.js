72c721017d71ebdd10776644eb897d6c3fa4ff87
leo <leo@users.noreply.github.com>
2015-12-19 22:15:48 +0100

    Open URL directly

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const server = require('gulp-express');
 
 gulp.task( 'es6', () => {
+
   return gulp.src( 'src/**/*.js' )
 
   .pipe(sourcemaps.init())
@@ -16,9 +17,11 @@ gulp.task( 'es6', () => {
 
   .pipe( gulp.dest( 'dist' ) )
   .pipe( server.notify() );
+
 });
 
 gulp.task( 'sass', () => {
+
   return gulp.src( 'src/**/*.scss' )
 
   .pipe(sourcemaps.init())
@@ -29,12 +32,12 @@ gulp.task( 'sass', () => {
 
   .pipe( gulp.dest('dist') )
   .pipe( server.notify() );
+
 });
 
 gulp.task( 'watch', () => {
 
   server.run( ['index.js'] );
-  open({uri: 'http://localhost:5670'})
 
   gulp.watch( 'src/*.html', server.notify );
   gulp.watch( 'src/*.scss', ['sass'] );
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = module.exports.app = exports.app = express();
+const opn = require('opn');
 
 app.use( require('connect-livereload')() );
 app.use( '/assets', express.static('dist') );
@@ -8,7 +9,12 @@ app.get('/', function(req, res) {
   res.sendFile( __dirname + '/src/index.html' );
 });
 
-app.listen(5670, function () {
-  var port = this.address().port;
-  console.log( 'Editor is running at http://localhost:%s', port );
+app.listen(5670, function() {
+
+  const port = this.address().port;
+  const url = 'http://localhost:' + port;
+
+  console.log( 'Editor is running at ' + url );
+  opn( url );
+
 });
